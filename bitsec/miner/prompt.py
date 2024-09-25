6fe8e238bf196aa86f4afae4fb471338ab233d22
yubrew <john@bitsec.ai>
2024-09-25 16:33:54 -0400

    fix md formatting bug, avoid burning tokens on default testnet runs

--- bitsec/miner/prompt.py
+++ bitsec/miner/prompt.py
@@ -3,8 +3,10 @@
 
 import openai
 import json
+import re
 from openai import OpenAI
 from bitsec.protocol import PredictionResponse
+from bitsec.utils.data import SAMPLE_DIR
 import bittensor as bt
 import os
 from tenacity import (
@@ -124,11 +126,28 @@ def format_analysis(
             temperature=temperature,
             max_tokens=max_tokens
         )
-        return response.choices[0].message.content
+        content = response.choices[0].message.content
+        content = re.sub(r'```json\s*|\s*```', '', content)
+        return content
     except Exception as e:
         bt.logging.error(f"Failed to format analysis: {e}")
         raise
 
+def default_testnet_code(code: str) -> bool:
+    file_path = os.path.join(SAMPLE_DIR, "nft-reentrancy.sol")
+
+    # Check if the file exists
+    if not os.path.exists(file_path):
+        return f"Error: The file '{file_path}' does not exist."
+    
+    # Read the contents of the file
+    try:
+        with open(file_path, 'r') as file:
+            file_contents = file.read()
+    except IOError:
+        return f"Error: Unable to read the file '{file_path}'."
+    
+    return file_contents == code
 
 def code_to_vulns(code: str) -> PredictionResponse:
     """
@@ -140,10 +159,16 @@ def code_to_vulns(code: str) -> PredictionResponse:
     Returns:
         PredictionResponse: The structured vulnerability report.
     """
+
+    ## short circuit testnet default code
+    if default_testnet_code(code) == True:
+      bt.logging.info("Default Testnet Code detected. Sending default prediction.")
+      return PredictionResponse.from_tuple([1.0,[]])
+
     try:
-        bt.logging.info("analyzing code")
+        bt.logging.info(f"analyzing code:\n{code}")
         analysis = analyze_code(code)
-        bt.logging.info("Analysis result:\n{analysis}")
+        bt.logging.info(f"Analysis result:\n{analysis}")
 
         formatted_result = format_analysis(analysis)
         bt.logging.debug(f"Formatted result: {formatted_result}")
