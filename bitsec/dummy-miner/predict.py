98de657841121ea26f04e354123367fcb20c1187
yubrew <john@bitsec.ai>
2025-01-15 15:01:30 -0600

    create dummy miner always returns false

---
+++ bitsec/dummy-miner/predict.py
@@ -0,0 +1,22 @@
+from bitsec.protocol import PredictionResponse
+from bitsec.miner.prompt import code_to_vulns
+
+## Predicting vulnerabilities is a multifaceted task. Here are some example improvements:
+# - train custom model
+# - use a more powerful foundational model
+# - improve prompt
+# - increase inference time compute
+# - augmented static analysis output
+def predict(code: str) -> PredictionResponse:
+    """
+    Perform prediction. You may need to modify this if you train a custom model.
+
+    Args:
+        code (str): The input str is a challenge that either has a severe code vulnerability or does not.
+
+    Returns:
+        PredictionResponse: The predicted output value and list of vulnerabilities.
+    """
+    
+
+    return code_to_vulns(code=code)
