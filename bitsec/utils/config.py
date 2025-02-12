597a76244c4dc9ab38672826c7457a0b37f121e6
yubrew <john@bitsec.ai>
2025-02-12 12:00:25 -0500

    default netuid 60

--- bitsec/utils/config.py
+++ bitsec/utils/config.py
@@ -68,7 +68,7 @@ def add_args(cls, parser):
     Adds relevant arguments to the parser for operation.
     """
 
-    parser.add_argument("--netuid", type=int, help="Subnet netuid", default=1)
+    parser.add_argument("--netuid", type=int, help="Subnet netuid", default=60)
 
     parser.add_argument(
         "--neuron.device",
