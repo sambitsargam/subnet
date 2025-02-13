97674b4a41bbd6f9c9879681db66426bcb0025e8
yubrew <john@bitsec.ai>
2025-02-12 20:54:23 -0500

    refactor scripts

--- start-miner.sh
+++ start-miner.sh
@@ -1,36 +1,8 @@
 #!/bin/bash
-NETUID=209 # Default to testnet
-
-
-# Kill previous instance if running
-PID_FILE="miner.pid"
-if [ -f "$PID_FILE" ]; then
-    OLD_PID=$(cat "$PID_FILE")
-    kill -9 $OLD_PID
-    sleep 1
-    rm "$PID_FILE"
-fi
-
-# Activate virtual environment
-source venv/bin/activate
-echo "Activated virtual environment"
-
-# Parse command-line arguments for netuid
-while [[ "$#" -gt 0 ]]; do
-  case $1 in
-    --netuid)
-      NETUID="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    *)
-      shift # past unrecognized argument
-      ;;
-  esac
-done
+NETUID=60 # Default to mainnet
 
 # Start miner and save PID
-python -m neurons.miner --netuid $NETUID --subtensor.chain_endpoint test \
+python -m neurons.miner --netuid $NETUID --subtensor.chain_endpoint finney \
     --wallet.name miner --wallet.hotkey default \
     --axon.port 8092 --axon.external_port 8092 \
     --logging.debug
--- start-validator.sh
+++ start-validator.sh
@@ -1,36 +1,6 @@
 #!/bin/bash
 NETUID=60 # Default to mainnet
 
-echo "Starting validator in ./scripts/start-validator.sh"
-
-# Kill previous instance if running
-PID_FILE="../validator.pid"
-if [ -f "$PID_FILE" ]; then
-    OLD_PID=$(cat "$PID_FILE")
-    kill -9 $OLD_PID
-    sleep 1
-    rm "$PID_FILE"
-fi
-
-echo "Activating virtual environment"
-source venv/bin/activate
-
-
-# Parse command-line arguments for netuid
-while [[ "$#" -gt 0 ]]; do
-  case $1 in
-    --netuid)
-      NETUID="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    *)
-      shift # past unrecognized argument
-      ;;
-  esac
-done
-
-
 echo "Starting validator with netuid $NETUID"
 python -m neurons.validator --netuid $NETUID --subtensor.chain_endpoint finney \
     --wallet.name validator --wallet.hotkey default \
